718b06f89a600150b740780fcb38b981523986fb
Aleksandr Epp <aleksandr.epp@gmail.com>
2021-10-22 15:44:57 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import dataSet from "data/"
+// Variables
+let skeleton; // Storing the skeleton data
+let positions; // Storing the joint positions
+let positions_scaled; // Storing the scaled joint positions
+const figureScale = 2; // The scaling factor for our visualizations
+const h = 200; // The height of the visualization
+const w = 400; // The width of the visualization
+
+function draw() {
+}
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "[name].js",
+  },
+  devtool: "source-map",
+  devServer: {
+    static: path.join(__dirname, "public"),
+    compress: true,
+    port: 9000,
+    allowedHosts: "all",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+      filename: "index.html",
+      chunks: ["main"],
+    }),
+  ],
+};
